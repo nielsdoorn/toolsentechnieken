5f987eaf7b02115324c973fb6c159a7333ffc47e
Remco <remco@Remcos-iMac.local>
2015-06-25 15:01:03 +0200

    Added tools page and nav menu

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
+  resources :tools
+
   # root 'welcome#index'
 
   # Example of regular route:
