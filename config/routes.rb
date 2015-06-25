192c00ced0aecfd168aa94a05bc000449466ab86
Remco <remco@Remcos-iMac.local>
2015-06-25 15:00:21 +0200

    Added home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  root 'pages#home'
+
   # root 'welcome#index'
 
   # Example of regular route:
